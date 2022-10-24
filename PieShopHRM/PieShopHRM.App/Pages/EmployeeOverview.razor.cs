e377bc2a126987c865187af1dc7625e7ad8292d8
Alex Jordany <alexjordany@outlook.com>
2022-10-23 21:24:09 -0600

    Add Employee Card and QuickViewPopup component

--- PieShopHRM/PieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ PieShopHRM/PieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -7,9 +7,16 @@ public partial class EmployeeOverview
 {
     public List<Employee>? Employees { get; set; } = default!;
 
+    private Employee? _selectedEmployee;
+
     protected override void OnInitialized()
     {
         Employees = MockDataService.Employees;
+    }
+
+    public void ShowQuickViewPopup(Employee selectedEmployee)
+    {
+        _selectedEmployee = selectedEmployee;
     }
 }
 
